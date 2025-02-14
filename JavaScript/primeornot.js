ba683fea38f4c371284202ce18af8c7ac788166c
uvapriya75 <uvapriya75@gmail.com>
2025-02-14 12:34:56 +0530

    Create primeornot.js

---
+++ JavaScript/primeornot.js
@@ -0,0 +1,15 @@
+const a= 21; // Change this value to test different numbers
+
+function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+if (isPrime(a)) {
+    console.log(`${a} is a prime number.`);
+} else {
+    console.log(`${a} is not a prime number.`);
+}
