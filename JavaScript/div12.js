c9c1c5bc02328d3c3b898b0ce05946edb3fba535
uvapriya75 <uvapriya75@gmail.com>
2025-02-14 12:46:38 +0530

    Create div12.js

---
+++ JavaScript/div12.js
@@ -0,0 +1,7 @@
+const num = 24; // Change this value to test different numbers
+
+if (num % 12 === 0) {
+    console.log(`${num} is divisible by 12.`);
+} else {
+    console.log(`${num} is not divisible by 12.`);
+}
