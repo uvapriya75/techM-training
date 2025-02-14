bb0571950e3d4abef3419f9095b41e5721379a86
uvapriya75 <uvapriya75@gmail.com>
2025-02-14 12:31:21 +0530

    Create PositiveNegative.js

---
+++ JavaScript/PositiveNegative.js
@@ -0,0 +1,9 @@
+var num = 29; // Change this value to test different cases
+
+if (num > 0) {
+    console.log(" positive.");
+} else if (num < 0) {
+    console.log("  negative.");
+} else {
+    console.log("The number is zero.");
+}
