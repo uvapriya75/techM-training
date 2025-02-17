b5c145db8db6504550c9a1db0edc0ff4ddbad595
uvapriya75 <uvapriya75@gmail.com>
2025-02-17 09:26:22 +0530

    Create Task6.js

---
+++ JavaScript/17-2-25/Task6.js
@@ -0,0 +1,12 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+console.log(person)
+
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
