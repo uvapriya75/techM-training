04db91069a3fb5d3caff9819d89abb5dbd0afd83
uvapriya75 <uvapriya75@gmail.com>
2025-02-23 19:30:50 +0530

    Update task10.java

--- JAVA/COREJAVA/Task3/task10.java
+++ JAVA/COREJAVA/Task3/task10.java
@@ -1,1 +1,19 @@
 
+package core;
+
+import java.io.*;
+
+public class ReadFileLineByLine {
+    public static void main(String[] args) {
+        String fileName = "C:\\Users\\uva\\OneDrive\\Documents\\photo\\uvaa.jpg"; // Change this to your file path
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
