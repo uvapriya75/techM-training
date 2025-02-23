76d18ecf24e3595aa3a1bbd3be6f25e8cb2144a3
uvapriya75 <uvapriya75@gmail.com>
2025-02-23 21:28:30 +0530

    Create task1.java

---
+++ JAVA/COREJAVA/Task7/task1.java
@@ -0,0 +1,16 @@
+package core2;
+import java.util.ArrayList;
+
+public class ColorList {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>();  
+        colors.add("Blue");
+        colors.add("Green");
+     colors.add("Purple");
+      colors.add("Red");
+  colors.add("Yellow");
+        // Print 
+        System.out.println("Color List: " + colors);
+    }
+}
