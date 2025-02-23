3cdd2f79cd996f993fee649073be4b0d640701c3
uvapriya75 <uvapriya75@gmail.com>
2025-02-23 22:43:09 +0530

    Update task1.java

--- JAVA/COREJAVA/Task9/task1.java
+++ JAVA/COREJAVA/Task9/task1.java
@@ -4,8 +4,7 @@ import java.util.LinkedList;
 public class LinkedListOccurrences {
     public static void main(String[] args) {
         LinkedList<String> pets = new LinkedList<>();
-        
-        // Adding elements to the linked list
+     
         pets.add("Dog");
         pets.add("Cat");
         pets.add("Rabbit");
@@ -15,12 +14,10 @@ public class LinkedListOccurrences {
         pets.add("Hamster");
         
         String target = "Cat";
-        
-        // Finding first and last occurrence
+     
         int firstIndex = pets.indexOf(target);
         int lastIndex = pets.lastIndexOf(target);
-        
-        // Displaying results
+    
         if (firstIndex != -1) {
             System.out.println("First occurrence of '" + target + "' is at index: " + firstIndex);
             System.out.println("Last occurrence of '" + target + "' is at index: " + lastIndex);
