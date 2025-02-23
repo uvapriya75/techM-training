207ff72dad403dc14b0aabca79905bbabeb44ae9
uvapriya75 <uvapriya75@gmail.com>
2025-02-23 20:32:47 +0530

    Update task1.java

--- JAVA/COREJAVA/Task4/task1.java
+++ JAVA/COREJAVA/Task4/task1.java
@@ -1,1 +1,16 @@
 
+package core2;
+
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        try {
+            
+            int result = 10 / 0;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            
+            System.out.println("Exception caught: Division by zero is not allowed.");
+        }
+        System.out.println("Program continues after exception handling.");
+    }
+}
