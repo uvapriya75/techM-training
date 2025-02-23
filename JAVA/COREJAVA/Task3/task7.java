0bc3cfd08b1bdb6e7a3c762491bf2585260376cf
uvapriya75 <uvapriya75@gmail.com>
2025-02-23 18:59:34 +0530

    Create task7.java

---
+++ JAVA/COREJAVA/Task3/task7.java
@@ -0,0 +1,23 @@
+package core;
+
+import java.util.Scanner;
+
+public class ConsoleInputExample {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+
+        scanner.close();
+    }
+}
