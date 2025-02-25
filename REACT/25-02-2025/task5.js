1d785ab219b7477db9228d7742e598ae47b823de
uvapriya75 <uvapriya75@gmail.com>
2025-02-25 11:17:18 +0530

    Create task5.js

---
+++ REACT/25-02-2025/task5.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function EvenNumber({ num }) {
+	return <h1>{num} is an Even Number!</h1>;
+}
+
+function OddNumber({ num }) {
+	return <h1>{num} is an Odd Number!</h1>;
+}
+
+function CheckNumber(props) {
+	const parsedNum = Number(props.num); // Convert string to number
+
+	if (isNaN(parsedNum)) {
+		return <h1>Invalid Number</h1>;
+	}
+
+	return parsedNum % 2 === 0 ? <EvenNumber num={parsedNum} /> : <OddNumber num={parsedNum} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CheckNumber num="90" />);
