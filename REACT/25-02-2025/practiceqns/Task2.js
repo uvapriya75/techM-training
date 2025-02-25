7fc0095a482fb14aa1d1d6c5568bd5ba8140fc60
uvapriya75 <uvapriya75@gmail.com>
2025-02-25 13:08:03 +0530

    Create Task2.js

---
+++ REACT/25-02-2025/practiceqns/Task2.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function NotDivisible({ num }) {
+  return <h1>{num} is not Divisible by 13!</h1>;
+}
+
+function Divisible({ num }) {
+  return <h1>{num} is Divisible by 13!</h1>;
+}
+
+function CheckNumber({ num }) {
+  if (num % 13 === 0) {
+    return <Divisible num={num} />;
+  } else {
+    return <NotDivisible num={num} />;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CheckNumber num={26} />);
