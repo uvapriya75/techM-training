6053eba8f032af6f082611d4567b3b46c9d38f33
uvapriya75 <uvapriya75@gmail.com>
2025-02-26 18:50:06 +0530

    Create task1.js

---
+++ REACT/26-02-2025/task1.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from "react-dom/client"
+
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage() {
+  const cars = ['Ford', 'BMW', 'Audi'];
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <ul>
+        {cars.map((car) => <Car brand={car} />)}
+      </ul>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage />);
