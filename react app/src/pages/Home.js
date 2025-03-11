00d62ff72ce9cc45efed267c82adb09b63e25c94
uvapriya75 <uvapriya75@gmail.com>
2025-03-11 11:52:28 +0530

    Create Home.js

---
+++ react app/src/pages/Home.js
@@ -0,0 +1,12 @@
+export const Home = () => (
+  <div className="text-center">
+    <h1>Welcome to Diet Planner</h1>
+    <h2>A balanced diet supplies the nutrients your body needs to work effectively. 
+        Without balanced nutrition, your body is more prone to disease, infection, fatigue, and low performance.
+        Children who don’t get enough healthy foods may face growth and developmental problems, poor academic performance, and frequent infections.
+        They can also develop unhealthy eating habits that may persist into adulthood.
+        Without exercise, they’ll also have a higher risk of obesity and various diseases that make up metabolic syndrome, such as type 2 diabetes and high blood pressure.
+        According to the Center for Science in the Public Interest, 4 of the top 10 leading causes of death in the United States are directly linked to diet.</h2>
+    <img src="https://via.placeholder.com/600x300" alt="Healthy Food" className="img-fluid mt-3" />
+  </div>
+);
