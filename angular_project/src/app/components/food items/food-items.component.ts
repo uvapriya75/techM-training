81393c8635a0b4e1a285ec59d38cce38c7b3a36d
uvapriya75 <uvapriya75@gmail.com>
2025-03-12 23:09:09 +0530

    Create food-items.component.ts

---
+++ angular_project/src/app/components/food items/food-items.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-food-item',
+  templateUrl: './food-item.component.html',
+  styleUrls: ['./food-item.component.css']
+})
+export class FoodItemComponent {
+  @Input() review: any;
+}
